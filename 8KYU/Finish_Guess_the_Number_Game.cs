53a507d1d6691d08ac44f417ed232c8d1e1286a0
gerasimenko <gerasimenko.serge@icloud.com>
2017-05-11 10:13:16 +0300

    Completed

---
+++ 8KYU/Finish_Guess_the_Number_Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Finish_Guess_the_Number_Game
+{
+    public class Guesser
+    {
+        private int number;
+        private int lives;
+
+        public Guesser(int number, int lives)
+        {
+            this.number = number;
+            this.lives = lives;
+        }
+
+        public bool Guess(int n)
+        {
+            if (this.lives <= 0) throw new Exception("Oooops");
+            var condition = (n == this.number);
+            if (!condition) this.lives--;
+            return condition;
+        }
+    }
+}
