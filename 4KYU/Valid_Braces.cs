3107b5d414ab20de14294a75ce1c9460f6f57da7
gerasimenko <gerasimenko.serge@icloud.com>
2017-05-11 11:22:53 +0300

    Completed

---
+++ 4KYU/Valid_Braces.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Kata
+{
+    public class Brace
+    {
+        public static bool validBraces(String braces)
+        {
+            int level = 0;
+            char[] c = braces.ToCharArray();
+            char[] check = new char[c.Length * 10];
+
+            for (int a = 0; a < c.Length; a++)
+            {
+                if (c[a] == '(' || c[a] == '{' || c[a] == '[') { level++; check[level] = c[a]; }
+                if (c[a] == ')' && check[level] == '(') { level--; } else { if (c[a] == ')' && check[level] != '(') { level = level + 10; } }
+                if (c[a] == '}' && check[level] == '{') { level--; } else { if (c[a] == '}' && check[level] != '{') { level = level + 10; } }
+                if (c[a] == ']' && check[level] == '[') { level--; } else { if (c[a] == ']' && check[level] != '[') { level = level + 10; } }
+            }
+
+            if (level == 0) { return true; } else { return false; }
+        }
+    }
+}
---
+++ 6KYU/Binary to Text (ASCII) Conversion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata
+{
+    class Binary_to_Text__ASCII__Conversion
+    {
+        public static string BinaryToString(string binary)
+        {
+            var byteText = new List<Byte>();
+
+            for (int i = 0; i < binary.Length; i += 8)
+            {
+                byteText.Add(Convert.ToByte(binary.Substring(i, 8), 2));
+            }
+
+            return Encoding.ASCII.GetString(byteText.ToArray());
+        }
+
+    }
+}
