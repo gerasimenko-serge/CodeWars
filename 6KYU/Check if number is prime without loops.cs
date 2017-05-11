03d7f6b380dddb078db8d7cecc4e1c7a0d7d7815
gerasimenko <gerasimenko.serge@icloud.com>
2017-05-11 18:04:01 +0300

    New kata. !Completed
    Нужна проверка на +-2

---
+++ 6KYU/Check if number is prime without loops.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+public class Kata
+{
+    public bool IsPrime(int number)
+    {
+        var t = Math.Abs(number);
+        var a = (int)t / 2;
+        var p = Enumerable.Range(2, (int)t / 2)
+            .Select(x => t % x != 0);
+        var p1 = p.Min();
+        return p1;
+    }
+}
