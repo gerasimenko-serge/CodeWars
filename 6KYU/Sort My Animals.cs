067c9b24c03b02dbbfd84caa21107edc9540d849
gerasimenko <gerasimenko.serge@icloud.com>
2017-05-29 14:41:59 +0300

    ttps://www.codewars.com/kata/58ff1c8b13b001a5a50005b4
    Write a method that accepts a list of objects of type Animal, and
    returns a new list. The new list should be a copy of the original list,
    sorted first by the animal's number of legs, and then by its name.
    
    If null is passed, the method should return null. If an empty list is
    passed, it should return an empty list back.

---
+++ 6KYU/Sort My Animals.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Animal
+{
+    public string Name { get; set; }
+
+    public int NumberOfLegs { get; set; }
+}
+
+public class AnimalSorter
+{
+    public List<Animal> Sort(List<Animal> input)
+    {
+        return input?.OrderBy(x => x.NumberOfLegs)
+            .ThenBy(x => x.Name).ToList();
+    }
+}
