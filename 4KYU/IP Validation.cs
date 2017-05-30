865f1c4b24438f8d50b0968f4edaf2c6006dc240
gerasimenko <gerasimenko.serge@icloud.com>
2017-05-30 10:05:30 +0300

    .

---
+++ 4KYU/IP Validation.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Clock_Kata
+{
+    public class Kata_V
+    {
+        private static readonly Regex validIpV4AddressRegex = new Regex(@"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]).){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$", RegexOptions.IgnoreCase);
+        public static Regex ValidIpV4AddressRegex => validIpV4AddressRegex;
+
+        public static bool is_valid_IP(string ipAddress)
+        {
+            if (!string.IsNullOrWhiteSpace(ipAddress))
+            {
+                return ValidIpV4AddressRegex.IsMatch(ipAddress.Trim());
+            }
+            return false;
+        }
+    }
+}
+        
---
+++ 6KYU/Clock in Mirror.cs
@@ -0,0 +1,12 @@
+﻿public class Kata
+{
+    public static string WhatIsTheTime(string timeInMirror)
+    {
+        var hourInMirror = 12 - int.Parse(timeInMirror.Substring(0, 2));
+        var minuteInMirror = (60 - int.Parse(timeInMirror.Substring(3))) % 60;
+        if (minuteInMirror != 0) --hourInMirror;
+        if (hourInMirror <= 0) hourInMirror += 12;
+        return $"{hourInMirror:00}:{minuteInMirror:00}";
+    }
+}
+        
