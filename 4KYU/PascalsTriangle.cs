0cf2c70445a8bd09f04b9b999d1a8508d29f31b5
gerasimenko <gerasimenko.serge@icloud.com>
2017-05-31 08:50:39 +0300

    Pascals Triangle

---
+++ 4KYU/PascalsTriangle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Clock_Kata
+{
+    public static class PascalsTriangleClass
+    {
+        public static List<int> PascalsTriangle(int n)
+        {
+            List<int> listTriangle = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int currentItem = 1;
+                for (int j = 0; j <= i; j++)
+                {
+                    listTriangle.Add(currentItem);
+                    currentItem = currentItem * (i - j) / (j + 1);
+                }
+            }
+            return listTriangle;
+        }
+    }
+
+
+}
+        
